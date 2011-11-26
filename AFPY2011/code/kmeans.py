b0817667947eb9b622768bff1c65046777c14772
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-11-26 18:52:08 +0100

    Cantine Numérique - AFPY 2011

---
+++ AFPY2011/code/kmeans.py
@@ -0,0 +1,209 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.cluster.k_means_ import KMeans
+from sklearn.datasets.samples_generator import make_blobs
+
+np.random.seed(0)
+
+batch_size = 45
+centers = [[1, 1], [-1, -1], [1, -1]]
+n_clusters = len(centers)
+X, labels_true = make_blobs(n_samples=1000, centers=centers, cluster_std=0.4)
+
+# Draw randoms
+indxs = np.arange(1000)
+np.random.shuffle(indxs)
+centroids = X[indxs[:3]]
+
+k_means = KMeans(k=3, max_iter=1, init=centroids)
+k_means.fit(X)
+k_means_labels1 = k_means.labels_
+k_means_cluster_centers1 = k_means.cluster_centers_
+
+k_means = KMeans(k=3, max_iter=2, init=centroids)
+k_means.fit(X)
+k_means_labels2 = k_means.labels_
+k_means_cluster_centers2 = k_means.cluster_centers_
+
+k_means = KMeans(k=3, max_iter=3, init=centroids)
+k_means.fit(X)
+k_means_labels3 = k_means.labels_
+k_means_cluster_centers3 = k_means.cluster_centers_
+
+k_means = KMeans(k=3, max_iter=4, init=centroids)
+k_means.fit(X)
+k_means_labels4 = k_means.labels_
+k_means_cluster_centers4 = k_means.cluster_centers_
+
+k_means = KMeans(k=3, max_iter=5, init=centroids)
+k_means.fit(X)
+k_means_labels5 = k_means.labels_
+k_means_cluster_centers5 = k_means.cluster_centers_
+
+k_means = KMeans(k=3, max_iter=6, init=centroids)
+k_means.fit(X)
+k_means_labels6 = k_means.labels_
+k_means_cluster_centers6 = k_means.cluster_centers_
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+fig = plt.figure(0)
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(X[:, 0], X[:, 1], ' ', marker='.',
+        markerfacecolor='#bbbbbb',
+        markeredgecolor='#111111')
+
+fig = plt.figure(1)
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(X[:, 0], X[:, 1], ' ', marker='.',
+        markerfacecolor='#bbbbbb',
+        markeredgecolor='#111111')
+
+
+for i in range(3):
+    ax.plot(centroids[i, 0], centroids[i, 1], colors[i],
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+fig = plt.figure(2)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels1 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(centroids[i, 0], centroids[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+fig = plt.figure(3)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels1 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers1[i, 0], k_means_cluster_centers1[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+fig = plt.figure(4)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels2 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers1[i, 0], k_means_cluster_centers1[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+fig = plt.figure(5)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels2 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers2[i, 0], k_means_cluster_centers2[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+
+fig = plt.figure(6)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels3 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers2[i, 0], k_means_cluster_centers2[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+
+fig = plt.figure(7)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels3 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers3[i, 0], k_means_cluster_centers3[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+
+fig = plt.figure(8)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels4 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers3[i, 0], k_means_cluster_centers3[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+
+fig = plt.figure(9)
+ax = fig.add_subplot(1, 1, 1)
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+for i in range(3):
+    my_members = k_means_labels4 == i
+    ax.plot(X[my_members, 0], X[my_members, 1], ' ', marker='.',
+        markerfacecolor=colors[i],
+        markeredgecolor='k')
+
+for i in range(3):
+    ax.plot(k_means_cluster_centers4[i, 0], k_means_cluster_centers4[i, 1], ' ',
+            marker='o',
+            markerfacecolor=colors[i],
+            markeredgecolor='k', markersize=6)
+
+
---
+++ AFPY2011/slides.rst
@@ -0,0 +1,94 @@
+================================================================================
+Scikit-learn: Segmenter une image
+================================================================================
+
+Plan
+------
+
+.. raw:: html
+
+    <span class="plan">
+
+- L'Apprentissage Statistique
+- Exemple: Reconnaissance Faciale
+
+-----
+
+La segmentation d'image
+--------------------------------------------------------------------------------
+
+  "La **segmentation d'image** est une opération de **traitement d'images**
+  qui a pour but de **rassembler** des pixels entre eux suivant des **critères
+  pré-définis**" -- *wikipedia*
+
+---------
+
+Scikit-learn: l'apprentissage statistique en python
+--------------------------------------------------------------------------------
+
+  "L'apprentissage automatique (machine learning en anglais), un des champs
+  d'étude de **l'intelligence artificielle**, est la discipline scientifique
+  concernée par **le développement, l'analyse et l'implémentation de méthodes
+  automatisables** qui permettent à une machine (au sens large) **d'évoluer
+  grâce à un processus d'apprentissage,** et ainsi de remplir des tâches qu'il
+  est difficile ou impossible de remplir par des moyens algorithmiques plus
+  classiques." -- **wikipedia**
+
+.. raw:: html
+
+  <span class="regression">
+
+.. image:: images/regression_linear.png
+  :scale: 85%
+
+.. raw:: html
+
+  </span>
+
+
+-----
+
+
+L'Apprentissage Supervisé
+--------------------------------------------------------------------------------
+
+ou analyse discriminante
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Production automatique de règles à partir d'une base de données contenant
+  des cas déjà traités et validés
+
+  - **Classification**
+  - **Regression**
+
+.. raw:: html
+
+  <span class="hyperplan">
+
+.. image:: images/hyperplane.png
+  :scale: 90%
+
+.. raw:: html
+
+  </span>
+
+
+--------------------------------------------------------------------------------
+
+
+L'Apprentissage Non Supervisé
+--------------------------------------------------------------------------------
+
+ou classification automatique
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+- Division d'un groupe de données en sous groupes de données similaires
+
+
+.. image:: images/clusters.png
+
+Le clustering avec KMeans
+================================================================================
+
+
